038d8428ebfc95895d49e9cb25c55adfafc1b577
youth-of-may <princess.giron@obf.ateneo.edu>
2024-02-24 07:33:10 +0800

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+public class Square implements DrawingObject{
+    private double x, y, size;
+    private Color color;
+
+    public Square(double x, double y, double size, Color color) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.color = color;
+    }
+
+    public void draw(Graphics2D g2d){
+
+    }
+    public void adjustX(double distance) {
+
+    }
+    public double getX(){
+        return x;
+    }
+}
