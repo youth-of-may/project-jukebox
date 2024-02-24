3be9a535bd2663ce6540ef3be846fb43ca52ab74
youth-of-may <princess.giron@obf.ateneo.edu>
2024-02-24 11:48:12 +0800

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.awt.geom.*;
+public class Rectangle implements DrawingObject{
+    private double x, y, width, height;
+    private Color color;
+    public Rectangle(double x, double y, double width, double height, Color color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    public void draw(Graphics2D g2d){
+        Rectangle2D.Double r1 = new Rectangle2D.Double(x, y, width, height);
+        g2d.setColor(color);
+        g2d.fill(r1);
+    }
+    public void adjustX(double distance) {
+
+    }
+    public double getX() {
+        return x;
+    }
+}
