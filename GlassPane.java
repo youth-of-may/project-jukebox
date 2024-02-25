e4f9162b0d4a7b86b416be2c41b2adf08de337fc
thisotherguyalso <161091156+thisotherguyalso@users.noreply.github.com>
2024-02-25 16:26:30 +0800

    Update GlassPane.java

--- GlassPane.java
+++ GlassPane.java
@@ -1,25 +1,24 @@
 import java.awt.*;
 import java.awt.geom.*;
 public class GlassPane implements DrawingObject {
-    private double x, y, x2, y2;
+    private double x, y, size;
 	
-    public GlassPane(double x, double y, double x2, double y2) {
+    public GlassPane(double x, double y, double size) {
 		this.x = x;
 		this.y = y;
-		this.x2 = x2;
-		this.y2 = y2;
+		this.size = size;
     }
     public void draw(Graphics2D g2d){
-        Rectangle r1 = new Rectangle(x,y,x2,15,new Color(171,187,184));
+        Rectangle r1 = new Rectangle(x,y,size,15,new Color(171,187,184));
 		r1.draw(g2d);
 		
-		Rectangle r2 = new Rectangle(x,y,15,y2,new Color(171,187,184));
+		Rectangle r2 = new Rectangle(x,y,15,size,new Color(171,187,184));
 		r2.draw(g2d);
 		
-		Rectangle r3 = new Rectangle(x2,y,15,y2,new Color(171,187,184));
+		Rectangle r3 = new Rectangle(size,y,15,size,new Color(171,187,184));
 		r3.draw(g2d);
 		
-		Rectangle r4 = new Rectangle(x,y2,x2+15,15,new Color(171,187,184));
+		Rectangle r4 = new Rectangle(x,size,size+15,15,new Color(171,187,184));
 		r4.draw(g2d);
 		
 		Square s1 = new Square(x+30,y+60,15,new Color(134,166,168));
@@ -31,10 +30,10 @@ public class GlassPane implements DrawingObject {
 		Square s3 = new Square(x+60,y+30,15,new Color(168,187,185));
 		s3.draw(g2d);
 		
-		Square s4 = new Square(x2-45,y2-30,15,new Color(168,187,185));
+		Square s4 = new Square(size-45,size-30,15,new Color(168,187,185));
 		s4.draw(g2d);
 		
-		Square s5 = new Square(x2-30,y2-45,15,new Color(168,187,185));
+		Square s5 = new Square(size-30,size-45,15,new Color(168,187,185));
 		s5.draw(g2d);
     }
     public void adjustX(double distance){
