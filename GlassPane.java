066271f1a084ff1185fa01b1d0d9bdcb226fc82d
thisotherguyalso <161091156+thisotherguyalso@users.noreply.github.com>
2024-02-25 16:39:25 +0800

    Update GlassPane.java

--- GlassPane.java
+++ GlassPane.java
@@ -15,10 +15,10 @@ public class GlassPane implements DrawingObject {
 		Rectangle r2 = new Rectangle(x,y,15,size,new Color(171,187,184));
 		r2.draw(g2d);
 		
-		Rectangle r3 = new Rectangle(size,y,15,size,new Color(171,187,184));
+		Rectangle r3 = new Rectangle(x+size,y,15,size,new Color(171,187,184));
 		r3.draw(g2d);
 		
-		Rectangle r4 = new Rectangle(x,size,size+15,15,new Color(171,187,184));
+		Rectangle r4 = new Rectangle(x,y+size,size+15,15,new Color(171,187,184));
 		r4.draw(g2d);
 		
 		Square s1 = new Square(x+30,y+60,15,new Color(134,166,168));
@@ -30,10 +30,10 @@ public class GlassPane implements DrawingObject {
 		Square s3 = new Square(x+60,y+30,15,new Color(168,187,185));
 		s3.draw(g2d);
 		
-		Square s4 = new Square(size-45,size-30,15,new Color(168,187,185));
+		Square s4 = new Square(x+size-45,y+size-30,15,new Color(168,187,185));
 		s4.draw(g2d);
 		
-		Square s5 = new Square(size-30,size-45,15,new Color(168,187,185));
+		Square s5 = new Square(x+size-30,y+size-45,15,new Color(168,187,185));
 		s5.draw(g2d);
     }
     public void adjustX(double distance){
