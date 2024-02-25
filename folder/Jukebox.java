e2f1751fe426a81be55a5fa4d0cf63536301e20d
thisotherguyalso <161091156+thisotherguyalso@users.noreply.github.com>
2024-02-25 11:05:12 +0800

    Update Jukebox.java

--- folder/Jukebox.java
+++ folder/Jukebox.java
@@ -12,27 +12,27 @@ public class Jukebox implements DrawingObject{
 	}
 	
 	public void draw(Graphics2D g2d){
-		Square s = new Square(x, y, size, new Color(24,23,18));
+		Square s = new Square(x, y, size+20, new Color(24,23,18));
 		s.draw(g2d);
 		
-		Square s2 = new Square(x+10, y+10, size-20, new Color(85,52,37));
+		Square s2 = new Square(x+10, y+10, size, new Color(85,52,37));
 		s2.draw(g2d);
 		
 		Path2D.Double line2 = new Path2D.Double();
 		line2.moveTo(300,380);
 		line2.lineTo(325,300);
-		line2.lineTo(475,300);
-		line2.lineTo(500,380);
+		line2.lineTo(495,300);
+		line2.lineTo(520,380);
 		line2.closePath();
 		g2d.setColor(new Color(24,23,18));
 		g2d.draw(line2);
 		g2d.fill(line2);
 		
 		Path2D.Double line3 = new Path2D.Double();
-		line3.moveTo(310,370);
+		line3.moveTo(315,370);
 		line3.lineTo(330,310);
-		line3.lineTo(470,310);
-		line3.lineTo(490,370);
+		line3.lineTo(485,310);
+		line3.lineTo(505,370);
 		line3.closePath();
 		g2d.setColor(new Color(85,52,37));
 		g2d.draw(line3);
