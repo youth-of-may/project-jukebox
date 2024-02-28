be32b84af1f5896fffac9a835732b62d8a75e4a0
thisotherguyalso <161091156+thisotherguyalso@users.noreply.github.com>
2024-02-28 10:33:42 +0800

    Create BoxDetails.java

---
+++ BoxDetails.java
@@ -0,0 +1,70 @@
+import java.awt.*;
+import java.awt.geom.*;
+import javax.swing.*;
+
+public class BoxDetails implements DrawingObject{
+	private double x, y;
+	
+	public BoxDetails(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void draw(Graphics2D g2d){
+		Rectangle r0 = new Rectangle(x-10,y,10,45,new Color(37,23,14));
+		r0.draw(g2d);
+		
+		Square s1 = new Square(x,y+15,15,new Color(37,23,14));
+		s1.draw(g2d);
+		
+		Square s2 = new Square(x+15,y,15,new Color(37,23,14));
+		s2.draw(g2d);
+		
+		Square s3 = new Square(x+15,y+30,15,new Color(37,23,14));
+		s3.draw(g2d);
+		
+		Rectangle r1 = new Rectangle(x+30,y+15,45,15,new Color(48,28,17));
+		r1.draw(g2d);
+	
+		Rectangle r2 = new Rectangle(x+45,y,15,45,new Color(37,23,14));
+		r2.draw(g2d);
+		
+		Square s4 = new Square(x+75,y,15,new Color(37,23,14));
+		s4.draw(g2d);
+		
+		Square s5 = new Square(x+75,y+30,15,new Color(37,23,14));
+		s5.draw(g2d);
+		
+		Rectangle r3 = new Rectangle(x+90,y+15,45,15,new Color(48,28,17));
+		r3.draw(g2d);
+	
+		Rectangle r4 = new Rectangle(x+105,y,15,45,new Color(37,23,14));
+		r4.draw(g2d);
+		
+		Square s6 = new Square(x+135,y,15,new Color(37,23,14));
+		s6.draw(g2d);
+		
+		Square s7 = new Square(x+135,y+30,15,new Color(37,23,14));
+		s7.draw(g2d);
+		
+		Rectangle r5 = new Rectangle(x+150,y+15,10,15,new Color(48,28,17));
+		r5.draw(g2d);
+		
+		Rectangle r6 = new Rectangle(x+150,y,10,15,new Color(67,44,30));
+		r6.draw(g2d);
+		
+		Rectangle r7 = new Rectangle(x+150,y+30,10,15,new Color(67,44,30));
+		r7.draw(g2d);
+		
+		Square s11 = new Square(x+135,y+15,15,new Color(67,44,30));
+		s11.draw(g2d);
+	}
+	
+	public void adjustX(double distance){
+        
+    }
+	
+    public double getX() {
+        return x;
+    }
+}
