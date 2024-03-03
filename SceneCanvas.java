d2aa16444752bf933d22e5c07a09beb5670db8a3
thisotherguyalso <161091156+thisotherguyalso@users.noreply.github.com>
2024-03-03 13:17:43 +0800

    Update SceneCanvas.java

--- SceneCanvas.java
+++ SceneCanvas.java
@@ -125,7 +125,10 @@ of the shapes in the list.
 		
 		Smoke smoke3 = new Smoke(600,y3);
 		smoke3.draw(g2d);
-		
+
+		MusicNote mn = new MusicNote(390,y4);
+		mn.draw(g2d);
+	    
 		if(y>max_y || y<0){
 			y--;
 			if (y==max_y){
