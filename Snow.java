69a5fb71b0a73ce60b8a099a29e4f000d7ce85d4
thisotherguyalso <161091156+thisotherguyalso@users.noreply.github.com>
2024-03-04 17:35:58 +0800

    Add files via upload

---
+++ Snow.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+
+public class Snow implements DrawingObject{
+	private double x,y;
+	
+	public Snow(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void draw(Graphics2D g2d){
+		Square s1 = new Square(x,y+5,5,Color.WHITE);
+		s1.draw(g2d);
+		
+		Square s2 = new Square(x+5,y,5,Color.WHITE);
+		s2.draw(g2d);
+		
+		Square s3 = new Square(x+10,y+5,5,Color.WHITE);
+		s3.draw(g2d);
+		
+		Square s4 = new Square(x+5,y+10,5,Color.WHITE);
+		s4.draw(g2d);
+	}
+	
+	public void adjustX(double distance){
+		
+	}
+	
+	public double getX(){
+		return x;
+	}
+}
---
+++ Snowfall.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+
+public class Snowfall implements DrawingObject{
+	private double x,y;
+	
+	public Snowfall(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void draw(Graphics2D g2d){
+		Snow sw = new Snow(x,y);
+		sw.draw(g2d);
+		
+		Snow sw2 = new Snow(x+20, y+30);
+		sw2.draw(g2d);
+		
+		Snow sw3 = new Snow(x+40, y+20);
+		sw3.draw(g2d);
+		
+		Snow sw4 = new Snow(x-10, y+30);
+		sw4.draw(g2d);
+	}
+	
+	public void adjustX(double distance){
+		
+	}
+	
+	public double getX(){
+		return x;
+	}
+}
